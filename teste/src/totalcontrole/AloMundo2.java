ff145ba71a5d064c255d42943262a0fb3025666b
Leda <fsutto@saojudas.br>
2012-07-10 00:47:20 -0300

    alomundo4

--- teste/src/totalcontrole/AloMundo2.java
+++ teste/src/totalcontrole/AloMundo2.java
@@ -5,6 +5,7 @@ public class AloMundo2 {
 	public static void main(String[] args) {
 		System.out.println("2 tentativa");
 		System.out.println("3 tentativa");
+		System.out.println("4 tentativa");
 	}
 	
 }
